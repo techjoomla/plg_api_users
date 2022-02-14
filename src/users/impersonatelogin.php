15111eba1140296d28f11250f342faf7da25ffbe
ttpllt46 <praneet_s@tekditechnologies.com>
2022-02-14 15:30:49 +0530

    Task #179501 feat: Impersonate Login API

--- src/users/impersonatelogin.php
+++ src/users/impersonatelogin.php
@@ -4,7 +4,7 @@
  * @subpackage  plg_api_users
  *
  * @author      Techjoomla <extensions@techjoomla.com>
- * @copyright   Copyright (C) 2009 - 2019 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
+ * @copyright   Copyright (C) 2009 - 2022 Techjoomla, Tekdi Technologies Pvt. Ltd. All rights reserved.
  * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  */
 
